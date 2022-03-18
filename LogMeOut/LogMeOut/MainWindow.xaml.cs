e9fce02cf2834f82095faf5381209af1a0bf5764
NekoLogi <nekologi@gmail.com>
2022-03-19 00:08:36 +0100

    v1.0.0

---
+++ LogMeOut/LogMeOut/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogMeOut
+{
+    public partial class MainWindow : Window
+    {
+        public static DateTime SHUTDOWN_DATE { get; set; }
+        private static bool IS_RUNNING { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private int[] GetValues()
+        {
+            try
+            {
+                var values = new List<int>
+                {
+                    int.Parse(Combobox_Days.SelectedItem.ToString()),
+                    int.Parse(Combobox_Hours.SelectedItem.ToString()),
+                    int.Parse(Combobox_Minutes.SelectedItem.ToString())
+                };
+                return values.ToArray();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return null;
+        }
+
+        private void CreateComboItems()
+        {
+            for (int i = 0; i < 30 + 1; i++)
+                Combobox_Days.Items.Add(i.ToString());
+            for (int i = 0; i < 24 + 1; i++)
+                Combobox_Hours.Items.Add(i.ToString());
+            for (int i = 0; i < 60 + 1; i++)
+                Combobox_Minutes.Items.Add(i.ToString());
+
+            Combobox_Days.SelectedIndex = 0;
+            Combobox_Hours.SelectedIndex = 0;
+            Combobox_Minutes.SelectedIndex = 0;
+        }
+
+        private void ChangeDate()
+        {
+            if (IS_RUNNING)
+            {
+                DateTime d1 = DateTime.Now;
+                DateTime d2 = DateTime.Now;
+
+                if (Combobox_Days.SelectedItem.ToString() != "0")
+                {
+                    d2 = d1.AddDays(double.Parse(Combobox_Days.SelectedItem.ToString()));
+                    d1 = d2;
+                }
+                if (Combobox_Hours.SelectedItem.ToString() != "0")
+                {
+                    d2 = d1.AddHours(double.Parse(Combobox_Hours.SelectedItem.ToString()));
+                    d1 = d2;
+                }
+                if (Combobox_Minutes.SelectedItem.ToString() != "0")
+                {
+                    d2 = d1.AddMinutes(double.Parse(Combobox_Minutes.SelectedItem.ToString()));
+                }
+                SHUTDOWN_DATE = d2;
+                Text_Datetime.Text = SHUTDOWN_DATE.ToString();
+            }
+        }
+
+
+
+        #region UI
+        private void Button_Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (GetValues() == null)
+                return;
+
+            string message;
+            int[] values = GetValues();
+
+            if ((bool)Checkbox_Restart.IsChecked)
+                message = Timer.RunTimer(values[0], values[1], values[2], Timer.Mode.Restart);
+            else
+                message = Timer.RunTimer(values[0], values[1], values[2], Timer.Mode.Shutdown);
+
+            MessageBox.Show(message);
+        }
+
+        private void OnComboBoxChange(object sender, SelectionChangedEventArgs e)
+        {
+            ChangeDate();
+        }
+
+        private void Button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            string message = Timer.RunTimer(0, 0, 0, Timer.Mode.Cancel);
+            MessageBox.Show(message);
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            CreateComboItems();
+            IS_RUNNING = true;
+            ChangeDate();
+        }
+
+        #endregion
+    }
+}
---
+++ LogMeOut/LogMeOut/Timer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogMeOut
+{
+    internal class Timer
+    {
+        public enum Mode
+        {
+            Shutdown,
+            Restart,
+            Cancel
+        }
+
+
+        public static string RunTimer(int days, int hours, int minutes, Mode mode)
+        {
+            switch (mode)
+            {
+                case Mode.Shutdown:
+                    return ShutdownPC(days, hours, minutes);
+                case Mode.Restart:
+                    return RestartPC(days, hours, minutes);
+                case Mode.Cancel:
+                    return CancelTimer();
+            }
+            return null;
+        }
+
+
+        private static string ShutdownPC(int days, int hours, int minutes)
+        {
+            int seconds = GetSeconds(days, hours, minutes);
+            RunCommand("/s /f /t", seconds);
+            return ReturnMessage(days, hours, minutes, Mode.Shutdown);
+        }
+
+        private static string CancelTimer()
+        {
+            var process = new System.Diagnostics.ProcessStartInfo("shutdown", "/a")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            System.Diagnostics.Process.Start(process);
+
+            return "Timer has been cancelled.";
+        }
+
+        private static string RestartPC(int days, int hours, int minutes)
+        {
+            int seconds = GetSeconds(days, hours, minutes);
+            RunCommand("/r /f /t", seconds);
+            return ReturnMessage(days, hours, minutes, Mode.Restart);
+        }
+
+        private static string ReturnMessage(int days, int hours, int minutes, Mode mode)
+        {
+            return $"PC will {mode} in {days} days {hours} hours {minutes} minutes.\n" +
+                $"Expected Shutdown: {MainWindow.SHUTDOWN_DATE}";
+        }
+
+        private static int GetSeconds(int days, int hours, int minutes)
+        {
+            int seconds = 0;
+            for (int i = 0; i < days; i++)
+                seconds += 86400;
+            for (int i = 0; i < hours; i++)
+                seconds += 3600;
+            for (int i = 0; i < minutes; i++)
+                seconds += 60;
+
+            return seconds;
+        }
+
+        private static void RunCommand(string mode, int seconds)
+        {
+            //System.Diagnostics.Process process1 = new System.Diagnostics.Process();
+            //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+            //{
+            //    FileName = "cmd.exe",
+            //    Arguments = $"{command}",
+            //    CreateNoWindow = false,
+            //    RedirectStandardInput = true,
+            //    RedirectStandardOutput = true,
+            //};
+            //process1.StartInfo = startInfo;
+            //process1.Start();
+
+            var process = new System.Diagnostics.ProcessStartInfo("shutdown", $"{mode} {seconds}")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            System.Diagnostics.Process.Start(process);
+        }
+    }
+}
